9aac13354de597de0ce17e173c305c5206335f72
httpdigest <kburjack@googlemail.com>
2018-07-11 19:57:21 +0200

    Mavenize the project and remove usage of internal sun API

---
+++ src/test/java/lwjgui/BlurPaneExample.java
@@ -0,0 +1,177 @@
+package lwjgui;
+
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.system.MemoryStack.stackMallocFloat;
+import static org.lwjgl.system.MemoryStack.stackPop;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
+import java.io.IOException;
+import java.nio.FloatBuffer;
+
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+
+import lwjgui.Color;
+import lwjgui.LWJGUI;
+import lwjgui.LWJGUIUtil;
+import lwjgui.geometry.Insets;
+import lwjgui.gl.GenericShader;
+import lwjgui.gl.Renderer;
+import lwjgui.scene.Context;
+import lwjgui.scene.Scene;
+import lwjgui.scene.Window;
+import lwjgui.scene.control.Label;
+import lwjgui.scene.layout.BlurPane;
+import lwjgui.scene.layout.BorderPane;
+
+public class BlurPaneExample {
+	public static final int WIDTH   = 320;
+	public static final int HEIGHT  = 240;
+
+	public static void main(String[] args) throws IOException {
+		if ( !glfwInit() )
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		// Create a standard opengl 3.2 window. You can do this yourself.
+		long window = LWJGUIUtil.createOpenGLCoreWindow("OpenGL Example", WIDTH, HEIGHT, true, false);
+
+		// Initialize lwjgui for this window
+		Window newWindow = LWJGUI.initialize(window);
+		Scene scene = newWindow.getScene();
+
+		// Add some components
+		addComponents(scene);
+
+		// Add a rendering callback to the window. This is the first thing called when the window draws.
+		newWindow.setRenderingCallback(new RenderingCallbackTest());
+
+		// Game Loop
+		while (!GLFW.glfwWindowShouldClose(window)) {
+			// Render GUI
+			LWJGUI.render();
+		}
+
+		// Stop GLFW
+		glfwTerminate();
+	}
+
+	private static void addComponents(Scene scene) {
+		// Create a simple pane
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(16,16,16,16));
+		root.setBackground(null);
+		scene.setRoot(root);
+
+		// Put a Blurred Pane in the scene.
+		// The blurred pane works by taking anything drawn underneath it in
+		// the same FBO, and blurs the section of it that the pane overlaps.
+		BlurPane pane = new BlurPane();
+		pane.setPrefSize(150, 150);
+		root.setCenter(pane);
+		
+		// Add a label
+		Label l = new Label("Hello World!");
+		l.setTextFill(Color.WHITE_SMOKE);
+		pane.getChildren().add(l);
+	}
+
+	/**
+	 * This is a generic testing class to test OpenGL Rendering
+	 * @author Andrew
+	 *
+	 */
+	private static class RenderingCallbackTest implements Renderer {
+		private GenericShader shader;
+		private int vao;
+		private int vbo;
+		private float rot;
+
+		public RenderingCallbackTest() {
+			// Test shader
+			shader = new GenericShader(); // Will load a testing vert/frag quad shader
+			
+			// Setup geometry
+			int vertSize = 3; // vec3 in shader
+			int texSize = 2; // vec2 in shader
+			int colorSize = 4; // vec4 in shader
+			int size = vertSize + texSize + colorSize; // Stride length
+			int verts = 3; // Number of vertices
+			int bytes = Float.BYTES; // Bytes per element (float)
+			
+			stackPush();
+			{
+				// Initial vertex data
+				FloatBuffer buffer = stackMallocFloat(verts * size);
+				buffer.put(-0.5f).put(+0.5f).put(0.0f);		// Vert 1 position
+				buffer.put(new float[] {0.0f, 0.0f});		// Vert 1 texture
+				buffer.put(new float[] {1.0f,0.0f,0.0f,1.0f}); // Vert 1 color
+				
+				buffer.put(+0.5f).put(+0.5f).put(0.0f);		// Vert 2 position
+				buffer.put(new float[] {0.0f, 0.0f});		// Vert 2 texture
+				buffer.put(new float[] {0.0f,1.0f,0.0f,1.0f}); // Vert 2 color
+				
+				buffer.put(+0.0f).put(-0.5f).put(0.0f);		// Vert 3 position
+				buffer.put(new float[] {0.0f, 0.0f});		// Vert 3 texture
+				buffer.put(new float[] {0.0f,0.0f,1.0f,1.0f}); // Vert 3 color
+				buffer.flip();
+
+				// Generate buffers
+				vbo = glGenBuffers();
+				vao = glGenVertexArrays();
+
+				// Upload Vertex Buffer
+				glBindBuffer(GL_ARRAY_BUFFER, vbo);
+				glBufferData(GL_ARRAY_BUFFER, buffer, GL_STATIC_DRAW);
+
+				// Set attributes (automatically stored to currently bound VAO)
+				glBindVertexArray(vao);
+				glEnableVertexAttribArray(0); // layout 0 shader
+				glEnableVertexAttribArray(1); // layout 1 shader
+				glEnableVertexAttribArray(2); // layout 2 shader
+				int vertOffset = 0;
+				glVertexAttribPointer( 0, vertSize,  GL_FLOAT, false, size*bytes, vertOffset );
+				int texOffset = vertSize*bytes;
+				glVertexAttribPointer( 1, texSize,   GL_FLOAT, false, size*bytes, texOffset );
+				int colorOffset = texOffset + texSize*bytes;
+				glVertexAttribPointer( 2, colorSize, GL_FLOAT, false, size*bytes, colorOffset );
+
+				// Unbind
+				glBindBuffer(GL_ARRAY_BUFFER, 0);
+				glBindVertexArray(0);
+			}
+			stackPop();
+		}
+
+		private long lastTime = System.currentTimeMillis();
+		@Override
+		public void render(Context context) {
+			
+			// Calculate delta
+			long delta = System.currentTimeMillis()-lastTime;
+			lastTime = System.currentTimeMillis();
+			
+			// Calculate not rotation for triangle
+			rot += delta * 1.0e-3;
+			
+			// Bind shader for drawing
+			shader.bind();
+			shader.projectOrtho( -0.6f, -0.6f, 1.2f, 1.2f );
+			shader.setWorldMatrix(new Matrix4f().rotateY(rot));
+
+			// Disable culling (just in case)
+			GL11.glDisable(GL11.GL_CULL_FACE);
+			
+			// Render geometry
+			glBindVertexArray(vao);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		}
+	}
+}

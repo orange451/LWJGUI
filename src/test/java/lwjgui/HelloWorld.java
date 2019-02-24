8a6e5f3780b4751dcc29470c367bc267c56e10da
orange451 <drew.hamilton@mac.com>
2019-02-23 19:05:01 -0500

    Change HelloWorld example to using automatic window creation approach

--- src/test/java/lwjgui/HelloWorld.java
+++ src/test/java/lwjgui/HelloWorld.java
@@ -1,48 +1,28 @@
 package lwjgui;
 
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
 
-import java.io.IOException;
-
-import org.lwjgl.glfw.GLFW;
-
-import lwjgui.LWJGUI;
-import lwjgui.LWJGUIUtil;
+import lwjgui.LWJGUIProgram;
 import lwjgui.scene.Scene;
 import lwjgui.scene.Window;
 import lwjgui.scene.control.Label;
 import lwjgui.scene.layout.StackPane;
 
-public class HelloWorld {
+public class HelloWorld extends LWJGUIProgram {
 	public static final int WIDTH   = 320;
 	public static final int HEIGHT  = 240;
 
-	public static void main(String[] args) throws IOException {
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Create a standard opengl 3.2 window. You can do this yourself.
-		long window = LWJGUIUtil.createOpenGLCoreWindow("Hello World", WIDTH, HEIGHT, true, false);
-		
-		// Initialize lwjgui for this window
-		Window newWindow = LWJGUI.initialize(window);
-		Scene scene = newWindow.getScene();
-		
-		// Add some components
-		addComponents(scene);
-		
-		// Game Loop
-		while (!GLFW.glfwWindowShouldClose(window)) {
-			// Render GUI
-			LWJGUI.render();
-		}
-		
-		// Stop GLFW
-		glfwTerminate();
+	public static void main(String[] args) {
+		launch(args);
 	}
+	
+	@Override
+	public void init(String[] args, Window window) {
+		// Get the main scene of the window
+		Scene scene = window.getScene();
 
-	private static void addComponents(Scene scene) {
+		// Mark window as non-resizable
+		window.setResizible(false);
+		
 		// Create a simple pane
 		StackPane pane = new StackPane();
 		
@@ -50,16 +30,26 @@ public class HelloWorld {
 		scene.setRoot(pane);
 		
 		// Put a label in the pane
-		Label label = new Label("Hello World!");
-		
-		label.setOnMouseEntered(e -> {
-			System.out.println("Mouse entered Hello World label.");
-		});
-		
-		label.setOnMouseExited(e -> {
-			System.out.println("Mouse exited Hello World label.");
-		});
-		
-		pane.getChildren().add(label);
+		pane.getChildren().add(new Label("Hello World!"));
+	}
+
+	@Override
+	public void run() {
+		//
+	}
+
+	@Override
+	public String getProgramName() {
+		return "Hello World Application";
+	}
+
+	@Override
+	public int getDefaultWindowWidth() {
+		return WIDTH;
+	}
+
+	@Override
+	public int getDefaultWindowHeight() {
+		return HEIGHT;
 	}
 }

8e2d256986bf511c1306274a2ed94f6a9507fd0c
orange451 <drew.hamilton@mac.com>
2019-02-25 01:27:10 -0500

    Changed GridPaneExample

--- src/test/java/lwjgui/GridPaneExample.java
+++ src/test/java/lwjgui/GridPaneExample.java
@@ -1,15 +1,6 @@
 package lwjgui;
 
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-
-import java.io.IOException;
-
-import org.lwjgl.glfw.GLFW;
-
-import lwjgui.LWJGUI;
-import lwjgui.LWJGUIUtil;
-import lwjgui.scene.Scene;
+import lwjgui.geometry.Pos;
 import lwjgui.scene.Window;
 import lwjgui.scene.control.Button;
 import lwjgui.scene.control.Label;
@@ -18,37 +9,19 @@ import lwjgui.scene.control.text_input.TextField;
 import lwjgui.scene.layout.GridPane;
 import lwjgui.scene.layout.StackPane;
 
-public class GridPaneExample {
+public class GridPaneExample extends LWJGUIApplication {
 	public static final int WIDTH   = 320;
 	public static final int HEIGHT  = 240;
 
-	public static void main(String[] args) throws IOException {
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Create a standard opengl 3.2 window. You can do this yourself.
-		long window = LWJGUIUtil.createOpenGLCoreWindow("Grid Pane Example", WIDTH, HEIGHT, true, false);
-		
-		// Initialize lwjgui for this window
-		Window lwjguiWindow = LWJGUI.initialize(window);
-		
-		// Add some components
-		addComponents(lwjguiWindow.getScene());
-		
-		// Game Loop
-		while (!GLFW.glfwWindowShouldClose(window)) {
-			// Render GUI
-			LWJGUI.render();
-		}
-		
-		// Stop GLFW
-		glfwTerminate();
+	public static void main(String[] args) {
+		launch(args);
 	}
 
-	private static void addComponents(Scene scene) {
+	@Override
+	public void start(String[] args, Window window) {
 		// Create a simple pane as root
 		StackPane root = new StackPane();
-		scene.setRoot(root);
+		window.getScene().setRoot(root);
 		
 		GridPane grid = new GridPane();
 		grid.setHgap(6);
@@ -57,7 +30,29 @@ public class GridPaneExample {
 		grid.add(new Label("Password:"), 0, 1);
 		grid.add(new TextField(), 1, 0);
 		grid.add(new PasswordField(), 1, 1);
-		grid.add(new Button("Login"), 1, 3);
+		Button b = new Button("Login");
+		b.setAlignment(Pos.CENTER_RIGHT);
+		grid.add(b, 1, 3);
 		root.getChildren().add(grid);
+	}
+
+	@Override
+	public void run() {
+		//
+	}
+
+	@Override
+	public String getProgramName() {
+		return "Grid Pane Example";
+	}
+
+	@Override
+	public int getDefaultWindowWidth() {
+		return WIDTH;
+	}
+
+	@Override
+	public int getDefaultWindowHeight() {
+		return HEIGHT;
 	}
 }

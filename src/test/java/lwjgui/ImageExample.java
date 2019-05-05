b7424c6592c75352e0af6570be97384a88a486fc
Andrew Hamilton <drew.hamilton@mac.com>
2019-05-05 18:20:38 -0400

    Improved ImageView

--- src/test/java/lwjgui/ImageExample.java
+++ src/test/java/lwjgui/ImageExample.java
@@ -1,5 +1,6 @@
 package lwjgui;
 
+import lwjgui.geometry.Insets;
 import lwjgui.scene.Scene;
 import lwjgui.scene.Window;
 import lwjgui.scene.image.Image;
@@ -18,18 +19,20 @@ public class ImageExample extends LWJGUIApplication {
 	public void start(String[] args, Window window) {
 		// Create a simple pane
 		StackPane pane = new StackPane();
+		pane.setPadding(new Insets(8));
 		
 		// Create an image
 		Image img = new Image("lwjgui/flower.jpg");
 		
 		// Create a viewable pane for that image
 		ImageView view = new ImageView();
+		view.setPrefSize(128, 128);
 		view.setImage(img);
 		view.setMaintainAspectRatio(true);
 		pane.getChildren().add(view);
 		
 		// Set the scene
-		window.setScene(new Scene(pane, WIDTH, HEIGHT));
+		window.setScene(new Scene(pane));
 		window.show();
 	}
 

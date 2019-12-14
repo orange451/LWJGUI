ebba2cd38c62a3cbb2fdbcce33e976eedb3b8a6d
Andrew Hamilton <ahamilton@manh.com>
2019-12-14 14:01:10 -0500

    no message

--- src/main/java/lwjgui/style/StyleCornerRadius.java
+++
@@ -1,7 +0,0 @@
-package lwjgui.style;
-
-public interface StyleCornerRadius {
-	public float[] getCornerRadii();
-	public void setCornerRadii(float radius);
-	public void setCornerRadii(float cornerTopLeft, float cornerTopRight, float cornerBottomRight, float cornerBottomLeft);
-}

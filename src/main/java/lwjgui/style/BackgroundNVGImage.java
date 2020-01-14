bf0a5e788ec2e075ebb61b34b15fd0c01f56fbcb
orange451 <drew.hamilton@mac.com>
2020-01-13 23:33:48 -0500

    Added BackgroundNVGImage

---
+++ src/main/java/lwjgui/style/BackgroundNVGImage.java
@@ -0,0 +1,52 @@
+package lwjgui.style;
+
+import static org.lwjgl.system.MemoryStack.stackPush;
+
+import org.lwjgl.nanovg.NVGPaint;
+import org.lwjgl.nanovg.NanoVG;
+import org.lwjgl.system.MemoryStack;
+
+import lwjgui.LWJGUIUtil;
+import lwjgui.scene.Context;
+
+public class BackgroundNVGImage extends Background {
+
+	private int nvgImage;
+	
+	public BackgroundNVGImage() {
+		this(-1);
+	}
+	
+	public BackgroundNVGImage(int nvgImage) {
+		this.setNVGImage(nvgImage);
+	}
+
+	public int getNVGImage() {
+		return this.nvgImage;
+	}
+
+	public void setNVGImage(int image) {
+		this.nvgImage = image;
+	}
+
+	@Override
+	public void render(Context context, double x, double y, double width, double height, float[] cornerRadii) {
+		if ( context == null )
+			return;
+		
+		if ( this.getNVGImage() < 0 )
+			return;
+		
+		try (MemoryStack stack = stackPush()) {
+			long vg = context.getNVG();
+			NVGPaint imagePaint = NanoVG.nvgImagePattern(vg,  (int) x, (int) y, (int) width, (int) height, 0, nvgImage, 1, NVGPaint.callocStack(stack));
+			NanoVG.nvgFillPaint(vg, imagePaint);
+			LWJGUIUtil.fillRoundRect(context, (int) x, (int) y, width, height, cornerRadii[0], cornerRadii[1], cornerRadii[2], cornerRadii[3], null);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "BackgroundNVGImage("+getNVGImage()+")";
+	}
+}
